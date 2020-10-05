bee4e1a22d0c9a6e92158b90204fa76f7a25b1ae
Saki_Odawara <maroneco1217@gmail.com>
2020-10-05 19:37:53 +0900

    押下した要素を特定し、削除

--- JS課題３/js/main.js
+++ JS課題３/js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 {
 	const oneTask = document.getElementById("task");
+	const tbody = document.querySelector("tbody");
 
 	const tasks = [];
 	let i = -1;
@@ -8,33 +9,41 @@
 	function addTask() {
 		document.getElementById("add").addEventListener("click", () => {
 			tasks.push({comment: oneTask.value, status: "作業中", status2: "削除"});
-			i++;
 			console.log(tasks);
-			const tr = document.createElement("tr");
-			tr.classList = i;
-			const td = document.createElement("td");
-			td.textContent = `${i}`;
-			tr.appendChild(td);
-			const td2 = document.createElement("td");
-			td2.textContent = `${tasks[i].comment}`;
-			tr.appendChild(td2);
+			while (tbody.firstElementChild) {
+				tbody.removeChild(tbody.firstElementChild);
+			}
+			tasks.forEach((task, index) => {
+				const tr = document.createElement("tr");
+				const td = document.createElement("td");
+				td.textContent = `${index}`;
+				tr.appendChild(td);
+				const td2 = document.createElement("td");
+				td2.textContent = `${task.comment}`;
+				tr.appendChild(td2);
 
-			const td3 = document.createElement("td");
-			td3.classList.add("working");
-			td3.textContent = `${tasks[i].status}`;
-			tr.appendChild(td3);
+				const td3 = document.createElement("td");
+				td3.classList.add("working");
+				td3.textContent = `${task.status}`;
+				tr.appendChild(td3);
 
-			const td4 = document.createElement("td");
-			td4.classList.add("d_button");
-			td4.textContent = `${tasks[i].status2}`;
-			tr.appendChild(td4);
-			document.querySelector("tbody").appendChild(tr);
+				const td4 = document.createElement("td");
+				td4.classList.add("d_button");
+				td4.textContent = `${task.status2}`;
+				tr.appendChild(td4);
+				tbody.appendChild(tr);
 
-			td4.addEventListener("click", () => {
-				const tres = document.querySelectorAll("tbody > tr")[i];
-				tres.remove();
-				i--;
+				td4.addEventListener("click", () => {
+					if (tasks.length === 1) {
+						index = 0;
+					}
+					const tres = document.querySelectorAll("tbody > tr")[index];
+					tres.remove();
+					tasks.splice(index, 1);
+					console.log(index);
+				});
 			});
+
 			oneTask.value = "";
 		});
 	}

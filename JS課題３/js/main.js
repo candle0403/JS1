15b2aa80e54ddaddaed6ea56915b597d6a3033dc
Saki_Odawara <maroneco1217@gmail.com>
2020-10-05 15:03:28 +0900

    タスク削除機能追加
    チェックよろしくお願いいたします！

--- JS課題３/js/main.js
+++ JS課題３/js/main.js
@@ -5,28 +5,38 @@
 	const tasks = [];
 	let i = -1;
 
-	document.getElementById("add").addEventListener("click", () => {
-		tasks.push({comment: oneTask.value, status: "作業中", status2: "削除"});
-		i++;
-		console.log(tasks);
-		const tr = document.createElement("tr");
-		const td = document.createElement("td");
-		td.textContent = `${i}`;
-		tr.appendChild(td);
-		const td2 = document.createElement("td");
-		td2.textContent = `${tasks[i].comment}`;
-		tr.appendChild(td2);
+	function addTask() {
+		document.getElementById("add").addEventListener("click", () => {
+			tasks.push({comment: oneTask.value, status: "作業中", status2: "削除"});
+			i++;
+			console.log(tasks);
+			const tr = document.createElement("tr");
+			tr.classList = i;
+			const td = document.createElement("td");
+			td.textContent = `${i}`;
+			tr.appendChild(td);
+			const td2 = document.createElement("td");
+			td2.textContent = `${tasks[i].comment}`;
+			tr.appendChild(td2);
 
-		const td3 = document.createElement("td");
-		td3.classList.add("working");
-		td3.textContent = `${tasks[i].status}`;
-		tr.appendChild(td3);
+			const td3 = document.createElement("td");
+			td3.classList.add("working");
+			td3.textContent = `${tasks[i].status}`;
+			tr.appendChild(td3);
 
-		const td4 = document.createElement("td");
-		td4.classList.add("d_button");
-		td4.textContent = `${tasks[i].status2}`;
-		tr.appendChild(td4);
-		document.querySelector("tbody").appendChild(tr);
-		oneTask.value = "";
-	});
+			const td4 = document.createElement("td");
+			td4.classList.add("d_button");
+			td4.textContent = `${tasks[i].status2}`;
+			tr.appendChild(td4);
+			document.querySelector("tbody").appendChild(tr);
+
+			td4.addEventListener("click", () => {
+				const tres = document.querySelectorAll("tbody > tr")[i];
+				tres.remove();
+				i--;
+			});
+			oneTask.value = "";
+		});
+	}
+	addTask();
 }

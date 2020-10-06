34f32c9be1c755346505569a9372eb15e3088db3
Saki_Odawara <maroneco1217@gmail.com>
2020-10-06 23:28:11 +0900

    一部の関数化以外修正（質問しています！）
    ・シングルクオートに変更
    ・定数名修正（td２→commentTd　など）
    ・不要なテンプレートリテラル除去

--- JS課題３/js/main.js
+++ JS課題３/js/main.js
@@ -1,50 +1,56 @@
-"use strict";
+'use strict';
 {
-	const oneTask = document.getElementById("task");
-	const tbody = document.querySelector("tbody");
+	const oneTask = document.getElementById('task');
+	const tbody = document.querySelector('tbody');
 
 	const tasks = [];
-	let i = -1;
 
-	function addTask() {
-		document.getElementById("add").addEventListener("click", () => {
-			tasks.push({comment: oneTask.value, status: "作業中", status2: "削除"});
-			console.log(tasks);
-			while (tbody.firstElementChild) {
-				tbody.removeChild(tbody.firstElementChild);
-			}
-			tasks.forEach((task, index) => {
-				const tr = document.createElement("tr");
-				const td = document.createElement("td");
-				td.textContent = `${index}`;
-				tr.appendChild(td);
-				const td2 = document.createElement("td");
-				td2.textContent = `${task.comment}`;
-				tr.appendChild(td2);
+	function writeTask() {
+		while (tbody.firstElementChild) {
+			tbody.removeChild(tbody.firstElementChild);
+		}
+		tasks.forEach((task, index) => {
+			const tr = document.createElement('tr');
+			const idNumber = document.createElement('td');
+			idNumber.textContent = index;
+			tr.appendChild(idNumber);
+			const commentTd = document.createElement('td');
+			commentTd.textContent = task.comment;
+			tr.appendChild(commentTd);
 
-				const td3 = document.createElement("td");
-				td3.classList.add("working");
-				td3.textContent = `${task.status}`;
-				tr.appendChild(td3);
+			const statusTd = document.createElement('td');
+			statusTd.classList.add('working');
+			statusTd.textContent = task.status;
+			tr.appendChild(statusTd);
 
-				const td4 = document.createElement("td");
-				td4.classList.add("d_button");
-				td4.textContent = `${task.status2}`;
-				tr.appendChild(td4);
-				tbody.appendChild(tr);
+			const deleteTd = document.createElement('td');
+			deleteTd.classList.add('d_button');
+			deleteTd.textContent = task.delete_btn;
+			tr.appendChild(deleteTd);
+			tbody.appendChild(tr);
 
-				td4.addEventListener("click", () => {
-					if (tasks.length === 1) {
-						index = 0;
-					}
-					const tres = document.querySelectorAll("tbody > tr")[index];
-					tres.remove();
-					tasks.splice(index, 1);
-					console.log(index);
-				});
+			deleteTd.addEventListener('click', () => {
+				if (tasks.length === 1) {
+					index = 0;
+				}
+				const tres = document.querySelectorAll('tbody > tr')[index];
+				tres.remove();
+				tasks.splice(index, 1);
+				console.log(index);
 			});
 
-			oneTask.value = "";
+			oneTask.value = '';
+		});
+	}
+
+	function addTask() {
+		document.getElementById('add').addEventListener('click', () => {
+			tasks.push({
+				comment: oneTask.value,
+				status: '作業中',
+				delete_btn: '削除',
+			});
+			writeTask();
 		});
 	}
 	addTask();
